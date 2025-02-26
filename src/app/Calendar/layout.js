fc5ca80761661da8925fa6a14711b9343a833235
leeminje0411 <mj12270411@gmail.com>
2025-02-26 15:27:55 +0900

    ssss

--- src/app/Calendar/layout.js
+++ src/app/Calendar/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import prisma from '../lib/prisma';
-import Schedule from './index'
+import Schedule from '../component/schedule';
 
 const AboutLayout = async ({ children }) => {
     const schedules = await prisma.schedule.findMany();
@@ -9,39 +9,10 @@ const AboutLayout = async ({ children }) => {
 
     return (
        
-              <div className='container mx-auto p-4'>
-            <h1 className='text-4xl text-center text-blue-500 m-5'>일정</h1>
-            {children? children : null}
-           {/* 테이블 컨테이너 */}
-                <div className="overflow-x-auto">
-                <table className="w-full border-collapse bg-white shadow-md rounded-lg overflow-hidden">
-                    {/* 테이블 헤더 */}
-                    <thead className="bg-blue-500 text-white text-center">
-                    <tr className="text-center">
-                        <th className="p-3 ">작성자</th>
-                        <th className="p-3 ">일정</th>
-                        <th className="p-3 ">날짜</th>
-                        <th className="p-3 ">시작 시간</th>
-                        <th className="p-3 ">종료 시간</th>
-                    </tr>
-                    </thead>
-
-                    {/* 테이블 바디 */}
-                    <tbody>
-                    {schedules.map((item) => (
-                        <tr key={item.id} className="border-b text-center hover:bg-gray-100">
-                            <td className="p-3">{item.writer}</td>
-                            <td className="p-3">{item.schedule}</td>
-                            <td className="p-3">{item.date}</td>
-                            <td className="p-3">{item.startTime}</td>
-                            <td className="p-3">{item.endTime}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-                </div>
-        </div>
+        <Schedule schedules={schedules} children={children}/>
     );
 };
 
 export default AboutLayout;
+
+export const revalidate = 1
